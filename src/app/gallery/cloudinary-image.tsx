459250a2082421df971162297a3e5a420ec0af8e
Chrsitian <christianlozano99@outlook.com>
2023-08-22 15:47:57 -0400

    adding favorites

--- src/app/gallery/cloudinary-image.tsx
+++ src/app/gallery/cloudinary-image.tsx
@@ -1,8 +1,42 @@
 "use client"
 
+import { Star } from "@/components/icons/star"
+import { FullStar } from "@/components/icons/fullstar"
 import { CldImage } from "next-cloudinary"
+import { setAsFavrioteAction } from "./actions";
+import { useTransition } from "react";
+import { SearchResult } from "./page";
 
-export function CloudinaryImage(props: any) {
+export function CloudinaryImage(props: any & { imageData: SearchResult; path: string }) {
+    const [transition, startTransition] = useTransition();
 
-    return <CldImage{...props} />;
+    const { imageData } = props;
+
+    const isFavorite = imageData.tags.includes("favorite");
+
+    return (
+        <div className="relative">
+            <CldImage {...props}
+                src={imageData.public_id} />
+            {isFavorite ? (
+                <FullStar
+                    onClick={() => {
+                        startTransition(() => {
+                            setAsFavrioteAction(imageData.public_id, false, props.path);
+                        });
+                    }}
+                    className="absolute top-2 right-2 hover:text-white text-red-500 cursor-pointer" />
+            ) : (
+                <Star
+                    onClick={() => {
+                        startTransition(() => {
+                            setAsFavrioteAction(imageData.public_id, true, props.path);
+                        });
+                    }}
+                    className="absolute top-2 right-2 hover:text-red-500 cursor-pointer" />
+            )}
+
+
+        </div>
+    );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from '@/components/ui/button'
+import { Star } from "@/components/icons/star"
+import Link from 'next/link'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -22,12 +24,15 @@ function SideMenu() {
             Manage
           </h2>
           <div className="space-y-1">
-            <Button variant="secondary" className="w-full justify-start flex gap-2">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-              </svg>
+            <Button asChild variant="ghost" className="w-full justify-start flex gap-2">
 
-              Gallery
+              <Link href="/gallery">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                </svg>
+
+                Gallery
+              </Link>
             </Button>
             <Button variant="ghost" className="w-full justify-start flex gap-2">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -36,18 +41,17 @@ function SideMenu() {
 
               Albums
             </Button>
-            <Button variant="ghost" className="w-full justify-start flex gap-2">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-              </svg>
+            <Button asChild variant="ghost" className="w-full justify-start flex gap-2">
+              <Link href="/favorites">
+                <Star />
 
-
-              Favriotes
+                Favriotes
+              </Link>
             </Button>
           </div>
         </div>
       </div>
-    </div>);
+    </div >);
 }
 
 export default function RootLayout({
